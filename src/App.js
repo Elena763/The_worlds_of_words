855055961d07bb88ced4e26d92cf05e5265e66b6
Elena763 <elena.evg@bk.ru>
2023-11-16 13:39:21 +0300

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 //import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
-import CardContainer from "./components/CardContainer/CardContainer.jsx";
-import Table from "./components/table/Table.jsx";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
         <Header />
-        <CardContainer />
-        <Table />
         <Footer />
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,50 @@
 import "./header.scss";
+import CardContainer from "../CardContainer/CardContainer.jsx";
+import Table from "../table/Table.jsx";
 import Space from "../../assets/icons/galaxy_icon.png";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="header">
-      <div className="menu">
-        <div className="logo">
-          <img
-            className="img"
-            src={Space}
-            alt="space"
-          ></img>
-          The world of words
-        </div>
+    <div className="main">
+      <Router>
+        <div className="header">
+          <div className="menu">
+            <Link to="/">
+              <div className="logo">
+                <img
+                  className="img"
+                  src={Space}
+                  alt="space"
+                ></img>
+                The world of words
+              </div>
+            </Link>
+          </div>
+          <div className="menu">
+            <div className="home">
+              <Link to="/home">Home</Link>
+            </div>
+            <div className="game">
+              <Link to="/game">Game</Link>
+            </div>
+          </div>
       </div>
-      <div className="menu">
-        <div className="home">Home</div>
-        <div className="game">Game</div>
-      </div>
+      <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
+function Home() {
+  return <div className="h_table"><Table /></div>;
+}
+
+function Game() {
+  return <div className="h_card"><CardContainer /></div>;
+}
+
 export default Header;
