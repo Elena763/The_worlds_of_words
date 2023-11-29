2087a64f1d8cfe0edd842821d90e7c3f1907e2c7
Elena763 <elena.evg@bk.ru>
2023-11-29 16:44:12 +0300

    Change validation

--- src/components/tablerow/Tablerow.jsx
+++ src/components/tablerow/Tablerow.jsx
@@ -1,5 +1,5 @@
 //import AddSave from "../Save/AddSave.jsx";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Delete from "./../../assets/icons/trash.svg";
 import Edit from "./../../assets/icons/pencil.svg";
 import Save from "./../../assets/icons/save.svg";
@@ -10,46 +10,104 @@ function Tablerow(props)
 {
   const [pressed, setPressed] = useState(false);
   const [state, setState] = useState(props);
+  const [english, setEnglish] = useState('');
+  const [transcription, setTranscription] = useState('');
+  const [russian, setRussian] = useState('');
+  const [tags, setTags] = useState('');
+  const [englishDirty, setEnglishDirty] = useState(false);
+  const [transcriptionDirty, setTranscriptionDirty] = useState(false);
+  const [russianDirty, setRussianDirty] = useState(false);
+  const [tagsDirty, setTagsDirty] = useState(false);
+  const [formValid, setFormValid] = useState(false);
   const [errorEnglish, setErrorEnglish] = useState(false);
   const [errorTranscription, setErrorTranscription] = useState(false);
   const [errorRussian, setErrorRussian] = useState(false);
   const [errorTags, setErrorTags] = useState(false);
+//Помечаем форму не валидной, чтобы заблокровать кнопку Сохранить
+  useEffect(() => {
+    if (errorEnglish || errorRussian || errorTranscription || errorTags) {
+      setFormValid(false)
+    } else {
+      setFormValid(true)
+    }
+  }, [errorEnglish, errorRussian, errorTranscription, errorTags])
+//Ошибки инпутов
+  const englishHandler = (e) =>{
+    setEnglish(e.target.value)
+    const re = /^[a-zA-Z\s]+$/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setErrorEnglish('Поле должно содержать только английские буквы')
+    } else {
+      setErrorEnglish()
+    }
+  }
 
-  const handleChangeInput = (event) => {
-    setState({
-      ...state,
-      [event.target.dataset.name]: event.target.value,
-    });
-    if (state.english !== "") {
-      setErrorEnglish(false);
+  const russianHandler = (e) =>{
+    setRussian(e.target.value)
+    const re = /^[а-яА-ЯёЁ]+$/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setErrorRussian('Поле должно содержать только русские буквы')
+    } else {
+      setErrorRussian()
     }
-    if (state.transcription !== "") {
-      setErrorTranscription(false);
+  }
+
+  const transcriptionHandler = (e) =>{
+    setTranscription(e.target.value)
+    const re = /^[\D]*$/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setErrorTranscription("Поле должно содержать только английские буквы и символы [, ], :, '")
+    } else {
+      setErrorTranscription()
     }
-    if (state.russian !== "") {
-      setErrorRussian(false);
+  }
+
+  const tagsHandler = (e) =>{
+    setTags(e.target.value)
+    const re = /^[a-zA-Zа-яА-ЯёЁ\s]+$/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setErrorTags('Поле должно содержать только буквы')
+    } else {
+      setErrorTags()
     }
-    if (state.tags !== "") {
-      setErrorTags(false);
+  }
+//Вывод ошибки
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'english':
+        setEnglishDirty(true)
+        break
+      case 'transcription':
+        setRussianDirty(true)
+        break
+      case 'russian':
+        setTranscriptionDirty(true)
+        break
+      case 'tags':
+        setTagsDirty(true)
+        break
     }
-  };
+  }
+
   const handlePressedSave = () => {
-    if (state.english === "") {
+    if (english === "" ) {
       setErrorEnglish(true);
     }
-    if (state.transcription === "") {
+    if (transcription === "") {
       setErrorTranscription(true);
     }
-    if (state.russian === "") {
+    if (russian === "") {
       setErrorRussian(true);
     }
-    if (state.tags === "") {
+    if (tags === "") {
       setErrorTags(true);
     }
-    if (state.english !== "" && state.transcription !== "" && state.russian!== "" && state.tags !== "") {
+    if (english !== "" && transcription !== "" && russian!== "" && tags !== "") {
       setPressed(!pressed);
+      console.log(english, transcription, russian, tags);
     }
   };
+
   const handleChangeCansel = () => {
     setState({
       ...props,
@@ -65,49 +123,61 @@ function Tablerow(props)
     <tr className="tablerow">
 
       <td className="rowEnglish">
+      {(englishDirty && errorEnglish) && <div className="errorMessage">{errorEnglish}</div>}
         {pressed ? (
           <input
           name="english"
           className={errorEnglish ? "inputError":"inputEnglish"}
           data-name={"english"}
-          value={state.english}
-          onChange={handleChangeInput} />
+          value={english}
+          onBlur={e => blurHandler(e)}
+          onChange={e => englishHandler(e)}
+          />
         ) : (
           state.english
         )}
       </td>
       <td className="rowTranscription">
+      {(transcriptionDirty && errorTranscription) && <div className="errorMessage">{errorTranscription}</div>}
         {pressed ? (
           <input
           name="transcription"
           className={errorTranscription ? "inputError":"inputTranscription"}
           data-name={"transcription"}
-          value={state.transcription}
-          onChange={handleChangeInput} />
+          value={transcription}
+          onBlur={e => blurHandler(e)}
+          onChange={e => transcriptionHandler(e)}
+          />
         ):(
             state.transcription
         )}
       </td>
       <td className="rowRussian">
+      {(russianDirty && errorRussian) && <div className="errorMessage">{errorRussian}</div>}
         {pressed ? (
           <input
           name="russian"
           className={errorRussian ? "inputError":"inputRussian"}
           data-name={"russian"}
-          value={state.russian}
-          onChange={handleChangeInput} />
+          value={russian}
+          onBlur={e => blurHandler(e)}
+          onChange={e => russianHandler(e)}
+          />
         ) : (
           state.russian
       )}
       </td>
       <td className="rowTags">
+      {(tagsDirty && errorTags) && <div className="errorMessage">{errorTags}</div>}
         {pressed ? (
           <input
           name="tags"
           className={errorTags ? "inputError":"inputTags"}
           data-name={"tags"}
-          value={state.tags}
-          onChange={handleChangeInput}/>
+          value={tags}
+          onBlur={e => blurHandler(e)}
+          onChange={e => tagsHandler(e)}
+          />
         ) : (
           state.tags
         )}
@@ -116,6 +186,7 @@ function Tablerow(props)
         <div>
         {pressed ? (
           <button
+            disabled={!formValid}
             className="rowSave"
             onClick={handlePressedSave}>
             <img className="save"
